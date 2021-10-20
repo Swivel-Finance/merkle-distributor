dcb76fcaf294d1e9cafb9dc444562e9e81a05c41
Julian Traversa <unitedterrorhots@gmail.com>
2021-10-20 14:26:25 -0400

    allowances override

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.6.11;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@jtraversa/contracts/token/ERC20/ERC20.sol';
 
 contract TestERC20 is ERC20 {
     mapping(address => mapping(address => uint256)) private override _allowances;
+
     constructor (string memory name_, string memory symbol_, uint amountToMint) ERC20(name_, symbol_) public {
         setBalance(msg.sender, amountToMint);
     }
