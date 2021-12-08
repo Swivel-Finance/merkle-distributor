09f6b880c3ad39a1070b3e99c5e2dbd29ce4e837
Julian Traversa <unitedterrorhots@gmail.com>
2021-12-08 13:54:32 -0500

    add public currentDropNonce

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -10,6 +10,8 @@ contract MerkleDistributor is IMerkleDistributor {
     address public immutable override admin;
     // Must set and replace msg.sender as admin
     address public immutable SWIVELMULTISIG = address(0);
+    uint256 public currentDropNonce;
+    // 
     bool public paused;
     // This is a packed array of booleans.
     mapping(uint256 => bytes32) public merkleRoot;
@@ -34,13 +36,8 @@ contract MerkleDistributor is IMerkleDistributor {
     /// @param from The address of the wallet containing tokens to distribute
     /// @param to The address that will receive any currently remaining distributions (will normally be the same as from)
     /// @param amount The amount of tokens in the new distribution
-    /// @param dropNonce The nonce of the drop that is currently being overwritten
     /// @param merkleRoot_ The merkle root associated with the new distribution
-    function iterateDistribution(address from, address to, uint256 amount, bytes32 merkleRoot_, uint256 dropNonce) external override onlyAdmin(admin) {
-        require(!isCancelled[dropNonce], 'Drop nonce already cancelled');
-        
-        // unpause redemptions
-        pause(false);
+    function iterateDistribution(address from, address to, uint256 amount, bytes32 merkleRoot_) external override onlyAdmin(admin) {      
         
         // remove current token balance
         IERC20 _token = IERC20(token);
@@ -51,12 +48,16 @@ contract MerkleDistributor is IMerkleDistributor {
         _token.transferFrom(from, address(this), amount);
         
         // cancel previous drop nonce / previous distribution
-        isCancelled[dropNonce] = true;
+        isCancelled[currentDropNonce] = true;
         
         // add the new distribution's merkleRoot
-        merkleRoot[(dropNonce+1)] = merkleRoot_;
+        merkleRoot[(currentDropNonce+1)] = merkleRoot_;
 
-        emit newDistribution(merkleRoot_, (dropNonce+1));
+        // unpause redemptions
+        pause(false);
+
+        // emit distribution event
+        emit newDistribution(merkleRoot_, (currentDropNonce+1));
     }
 
     function isClaimed(uint256 index, uint256 dropNonce) public view override returns (bool) {
@@ -73,16 +74,16 @@ contract MerkleDistributor is IMerkleDistributor {
         claimedBitMap[dropNonce][claimedWordIndex] = claimedBitMap[dropNonce][claimedWordIndex] | (1 << claimedBitIndex);
     }
 
-    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof, uint256 dropNonce) external unpaused() override {
-        require(!isClaimed(index, dropNonce), 'MerkleDistributor: Drop already claimed.');
-        require(!isCancelled[dropNonce], 'Drop nonce already cancelled');
+    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external unpaused() override {
+        require(!isClaimed(index, currentDropNonce), 'MerkleDistributor: Drop already claimed.');
+        require(!isCancelled[currentDropNonce], 'Drop nonce already cancelled');
 
         // Verify the merkle proof.
         bytes32 node = keccak256(abi.encodePacked(index, account, amount));
-        require(MerkleProof.verify(merkleProof, merkleRoot[dropNonce], node), 'MerkleDistributor: Invalid proof.');
+        require(MerkleProof.verify(merkleProof, merkleRoot[currentDropNonce], node), 'MerkleDistributor: Invalid proof.');
 
         // Mark it claimed and send the token.
-        _setClaimed(index, dropNonce);
+        _setClaimed(index, currentDropNonce);
         require(IERC20(token).transfer(account, amount), 'MerkleDistributor: Transfer failed.');
 
         emit Claimed(index, account, amount);
